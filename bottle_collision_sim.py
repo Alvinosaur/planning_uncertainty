c326b6b0faf57463931e95a0008fd96eaa61b0e2
Alvinosaur <ashek@andrew.cmu.edu>
2020-02-22 12:21:02 -0500

    functional arm initial position

--- bottle_collision_sim.py
+++ bottle_collision_sim.py
@@ -1,15 +1,18 @@
 import pybullet as p
 import time
 import pybullet_data
+import math
 
 GRAVITY = -9.81
 BASE_ID = -1
 N = 10000  # total simulation iterations
-test_N = 200  # iters for each test of a parameter
+test_N = 20000  # iters for each test of a parameter
+NUM_JOINTS = 7
+END_EFFECTOR_ID = 6
 
 # pybullet_data built-in models
 plane_urdf_filepath = "plane.urdf"
-arm_filepath = "xarm/xarm6_robot.urdf"
+arm_filepath = "kuka_iiwa/model.urdf"
 table_filepath = "table/table.urdf"
 
 # p.setTimeOut(max_timeout_sec)
@@ -22,6 +25,22 @@ class Bottle:
         self.mass = 1
         self.start_pos = [0.5, 0, table_height+.1]
         self.start_ori = [0, 0, 0, 1]
+
+class Arm:
+    def __init__(self):
+        # NOTE: taken from example: 
+        # https://github.com/bulletphysics/bullet3/blob/master/examples/pybullet/examples/inverse_kinematics.py
+        #lower limits for null space
+        self.ll = [-.967, -2, -2.96, 0.19, -2.96, -2.09, -3.05]
+        #upper limits for null space
+        self.ul = [.967, 2, 2.96, 2.29, 2.96, 2.09, 3.05]
+        #joint ranges for null space
+        self.jr = [5.8, 4, 5.8, 4, 5.8, 4, 6]
+        #restposes for null space
+        # self.rp = [0, 0, 0, 0.5 * math.pi, 0, -math.pi * 0.5 * 0.66, 0]
+        self.rp = [0, math.pi/2, 0, 0, 0, 0, 0]
+        #joint damping coefficents
+        self.jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
 
 class Test:
     def __init__(self, bottle_mass, lat_fric, roll_fric, spin_fric, bounce, 
@@ -37,7 +56,7 @@ class Test:
 
 def reset_arm(arm_id, pos, ori):
     # reset arm
-    p.resetBasePositionAndOrientation(objectUniqueId=arm_id, 
+    p.resetBasePositionAndOrientation(bodyUniqueId=arm_id, 
         posObj=pos,
         ornObj=ori)
 
@@ -50,13 +69,108 @@ def change_arm_params():
     return
 
 
-def run_sim():
-    for _ in range(test_N):
+def run_sim(arm):
+    t = 0
+    # p.setJointMotorControl2(bodyUniqueId=arm.arm_id, 
+    #         jointIndex=0, 
+    #         controlMode=p.VELOCITY_CONTROL,
+    #         targetVelocity = 20,
+    #         force = 20)
+    for i in range(test_N):
         p.stepSimulation()
         time.sleep(1./240.)
+        t = t + 0.01
 
 
-def main():
+# def run_all_tests():
+#     physics_client = p.connect(p.GUI)  # or p.DIRECT for nongraphical version
+
+#     # allows you to use pybullet_data package's existing URDF models w/out actually having them
+#     p.setAdditionalSearchPath(pybullet_data.getDataPath())
+#     p.setGravity(0,0,GRAVITY)
+#     planeId = p.loadURDF(plane_urdf_filepath)
+
+#     # table model
+#     table_start_pos = [0, 0, 0]
+#     table_id = p.loadURDF(table_filepath, table_start_pos, useFixedBase=True)
+#     min_table_bounds, max_table_bounds = p.getAABB(table_id)
+#     table_height = max_table_bounds[2]
+
+#     # robot arm
+#     arm = Arm()
+#     arm_start_pos = [0, 0, table_height]
+#     arm_start_ori = p.getQuaternionFromEuler([0, 0, 0])
+#     arm.arm_id = p.loadURDF(arm_filepath, arm_start_pos, arm_start_ori)
+#     if (p.getNumJoints(arm.arm_id) != NUM_JOINTS):
+#         print("Invalid number of joints. Expected: %d, Actual: %d", NUM_JOINTS, num_joints)
+#         exit()  # undefined
+#     for i in range(numJoints):
+#         p.resetJointState(arm.arm_id, i, [0]*NUM_JOINTS)
+    
+#     # arm sim params
+#     #trailDuration is duration (in seconds) after debug lines will be removed automatically
+#     trailDuration = 5
+
+#     # jointPoses = p.calculateInverseKinematics(
+#     #     arm_id, END_EFFECTOR_ID, pos, orn, ll, ul, jr, rp)
+
+#     # bottle
+#     bottle = Bottle(table_height)
+#     bottle_col_id = p.createCollisionShape(
+#         shapeType=p.GEOM_CYLINDER,
+#         radius=bottle.radius, 
+#         height=bottle.height)
+
+#     mass_tests = []
+#     lat_friction_tests = []
+#     roll_friction_tests = []
+#     spinning_friction_tests = []
+#     bounciness_tests = [] 
+#     contact_stiffness_tests = []
+#     contact_damping_tests = []
+#     inertia_tests = []  # changes center of mass
+    
+#     all_tests = []  # all test combos
+
+#     running = True
+
+#     for test in all_tests:
+#         bottle_id = p.createMultiBody(
+#             baseMass=bottle.mass, 
+#             baseInertialFramePosition=test.bottle_inertia,
+#             baseCollisionShapeIndex=bottle_col_id,
+#             basePosition=bottle.start_pos)
+#         p.changeDynamics(
+#             bodyUniqueId=bottle_id, 
+#             linkIndex=BASE_ID, 
+#             mass=test.bottle_mass,
+#             lateralFriction=test.lat_fric,
+#             spinningFriction=test.spin_fric,
+#             rollingFriction=test.roll_fric,
+#             restitution=test.bounce,
+#             contactStiffness=test.contact_stiffness,
+#             contactDamping=test.contact_dampness)
+
+#         run_sim(arm)
+
+#         # check if bottle fell over
+#         cube_pos, cube_ori = p.getBasePositionAndOrientation(bottle_id)
+#         is_fallen = check_is_fallen(cube_pos, cube_ori)
+#         if is_fallen: 
+#             # TODO: Mark in some table, true
+#             print("fell over")
+
+#         # remove bottle to not interfere with next test
+#         p.removeBody(bottle_id)
+        
+#         # reset other components of simulation
+#         reset_arm(arm_id=arm_id, pos=arm_start_pos, ori=arm_start_ori)
+
+    
+#     p.disconnect()
+
+
+def test_diff_factors():
     physics_client = p.connect(p.GUI)  # or p.DIRECT for nongraphical version
 
     # allows you to use pybullet_data package's existing URDF models w/out actually having them
@@ -71,59 +185,78 @@ def main():
     table_height = max_table_bounds[2]
 
     # robot arm
+    arm = Arm()
     arm_start_pos = [0, 0, table_height]
     arm_start_ori = p.getQuaternionFromEuler([0, 0, 0])
-    arm_id = p.loadURDF(arm_filepath, arm_start_pos, arm_start_ori)
+    arm.arm_id = p.loadURDF(arm_filepath, arm_start_pos, arm_start_ori)
+    if (p.getNumJoints(arm.arm_id) != NUM_JOINTS):
+        print("Invalid number of joints. Expected: %d, Actual: %d", NUM_JOINTS, num_joints)
+        exit()  # undefined
+    for i in range(NUM_JOINTS):
+        p.resetJointState(arm.arm_id, i, arm.rp[i])
+    
+    # arm sim params
+    #trailDuration is duration (in seconds) after debug lines will be removed automatically
+    trailDuration = 5
+
+    # bottle
     bottle = Bottle(table_height)
     bottle_col_id = p.createCollisionShape(
         shapeType=p.GEOM_CYLINDER,
         radius=bottle.radius, 
         height=bottle.height)
 
-    mass_tests = []
-    lat_friction_tests = []
-    roll_friction_tests = []
-    spinning_friction_tests = []
-    bounciness_tests = [] 
-    contact_stiffness_tests = []
-    contact_damping_tests = []
-    inertia_tests = []  # changes center of mass
-    
-    all_tests = []  # all test combos
+    test = Test(
+        bottle_mass = 1, 
+        lat_fric = 1, 
+        roll_fric = 1, 
+        spin_fric = 1, 
+        bounce = 1, 
+        contact_stiffness = 1, 
+        contact_dampness = 1, 
+        bottle_inertia = [0, 0, 0]
+    )
 
-    running = True
+    bottle_id = p.createMultiBody(
+        baseMass=bottle.mass, 
+        baseInertialFramePosition=test.bottle_inertia,
+        baseCollisionShapeIndex=bottle_col_id,
+        basePosition=bottle.start_pos)
+    p.changeDynamics(
+        bodyUniqueId=bottle_id, 
+        linkIndex=BASE_ID, 
+        # mass=test.bottle_mass,
+        # lateralFriction=test.lat_fric,
+        # spinningFriction=test.spin_fric,
+        # rollingFriction=test.roll_fric,
+        # restitution=test.bounce,
+        # contactStiffness=test.contact_stiffness,
+        # contactDamping=test.contact_dampness)
+    )
 
-    for test in all_tests:
-        bottle_id = p.createMultiBody(
-            baseMass=bottle.mass, 
-            baseInertialFramePosition=test.bottle_inertia,
-            baseCollisionShapeIndex=bottle_col_id,
-            basePosition=bottle.start_pos)
-        p.changeDynamics(
-            bodyUniqueId=bottle_id, 
-            linkIndex=BASE_ID, 
-            mass=test.bottle_mass,
-            lateralFriction=test.lat_fric,
-            spinningFriction=test.spin_fric,
-            rollingFriction=test.roll_fric,
-            restitution=test.bounce,
-            contactStiffness=test.contact_stiffness,
-            contactDamping=test.contact_dampness)
+    run_sim(arm)
 
-        run_sim()
-
-        # check if bottle fell over
-        cube_pos, cube_ori = p.getBasePositionAndOrientation(bottle_id)
-
-        # remove bottle to not interfere with next test
-        p.removeBody(bottle_id)
+    p.removeBody(bottle_id)
         
-        # reset other components of simulation
-        reset_arm(arm_id=arm_id, pos=arm_start_pos, ori=arm_start_ori)
+    # reset other components of simulation
+    reset_arm(arm_id=arm_id, pos=arm_start_pos, ori=arm_start_ori)
 
-    
+    run_sim()
+
     p.disconnect()
 
 
+def main():
+    # run_all_tests()
+    test_diff_factors()
+
 if __name__ == '__main__':
     main()
+
+
+
+    # cpu parallell pybullet
+    # ODE physics engine
+    # Vrep
+    # mujoco- fast, not accurate
+    # parallel Flex NVidia
